b383eb6646498e19a3b3f9a94a4d49bf5b94aa2e
Pedro <pjgomez_1997@tutanota.com>
2023-02-25 19:33:16 +0100

    Remove unused exceptions

--- src/main/java/org/warchest/exception/SquareOutOfRangeException.java
+++
@@ -1,7 +0,0 @@
-package org.warchest.exception;
-
-public class SquareOutOfRangeException extends Exception {
-    public SquareOutOfRangeException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/warchest/exception/UnitNotPresentException.java
+++
@@ -1,7 +0,0 @@
-package org.warchest.exception;
-
-public class UnitNotPresentException extends Exception {
-    public UnitNotPresentException(String message) {
-        super(message);
-    }
-}
